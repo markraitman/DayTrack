7eb19e9d0fb8bff919c86860bfde284ed8a40a4a
Mark Raitman <markraitman@mail.ru>
2024-12-20 19:18:15 +0300

    CalendarKit implementation
    - Implement CalendarKit
    - Fix bug issue with navigationBar appearance

---
+++ DayTrack/Controllers/CalendarViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DayTrack
+//
+//  Created by Марк Райтман on 16.12.2024.
+//
+
+import UIKit
+import CalendarKit
+
+class CalendarViewController: DayViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Calendar"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+    }
+
+}
+
